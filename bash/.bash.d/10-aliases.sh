e6c7a916b7d271ae5d35f35c876a4620ef314f26
Marco Wegner <673439+marvey11@users.noreply.github.com>
2026-09-04 11:16:13 +0000

    feat: use `eza` for `ls` aliases

--- bash/.bash.d/10-aliases.sh
+++ bash/.bash.d/10-aliases.sh
@@ -1,9 +1,15 @@
+# shellcheck shell=bash
+
 alias ..='cd ..'
 alias ...='cd ../..'
 alias grep='grep --color=auto'
-alias ls='ls --color=auto --group-directories-first'
-alias la='ls -Al'
-alias ll='ls -al'
+
+# Basic replacement for ls using eza
+alias ls='eza --group-directories-first'
+
+# Replacement for la / ll with long formatting, git status, and icons
+alias la='eza -la --group-directories-first --icons'
+alias ll='eza -l --group-directories-first --icons'
 
 # dotfiles command
 alias sys-check='system-check.sh'
