d37f840ae09ddd4d02220e70eda0b247d933c9a2
Marco Wegner <673439+marvey11@users.noreply.github.com>
2026-09-13 16:16:37 +0200

    fix: implement shellcheck fixes

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-TARGET_DIR="${1:-$HOME}"
+TARGET_DIR="${1:-${HOME}}"
 
-echo "Stowing packages from $DOTFILES_DIR to $TARGET_DIR..."
-stow --dir="$DOTFILES_DIR" --target="$TARGET_DIR" bash scripts git
+echo "Stowing packages from ${DOTFILES_DIR} to ${TARGET_DIR}..."
+stow --dir="${DOTFILES_DIR}" --target="${TARGET_DIR}" bash scripts git
